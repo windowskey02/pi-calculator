2f2fd28f11af24dc7e56b7f231d0eae05527bc99
windowskey02 <windowskey02@gmail.com>
2026-06-05 09:35:55 +0300

    Initial code

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int x;
+int y;
+cout << "choose a number" << endl;
+cin >> x;
+cout << "now choose another number" << endl;
+cin >> y;
+cout << "your results are in!" << endl;
+cout << x << " + " << y << " = " << x+y << endl;
+cout << x << " - " << y << " = " << x-y << endl;
+cout << x << " * " << y << " = " << x*y << endl;
+cout << x << " / " << y << " = " << x/y << endl;
+return 0;
+
+}
